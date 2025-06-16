2363374464701142d901cd7c5b849db01b5d641c
Solomon <soloabejo@gmail.com>
2025-06-16 17:41:33 +0800

    Updated class name from 'stack' to 'tools'; Added new tool elements

--- assets/js/about/technologies.js
+++ assets/js/about/technologies.js
@@ -1,26 +1,30 @@
-const stacks = [
-  { name: 'Linux', logo: './assets/images/tech/linux.svg' },
-  { name: 'Ansible', logo: './assets/images/tech/ansible.svg' },
-  { name: 'OpenStack', logo: './assets/images/tech/openstack.svg' },
-  { name: 'OpenShift', logo: './assets/images/tech/openshift.svg' },
-  { name: 'AWS', logo: './assets/images/tech/aws.svg' },
-  { name: 'Git', logo: './assets/images/tech/git.svg' },
-  { name: 'GitHub', logo: './assets/images/tech/github.svg' },
-  { name: 'GitLab', logo: './assets/images/tech/gitlab.svg' },
-  { name: 'Docker', logo: './assets/images/tech/docker.svg' },
-  { name: 'Kubernetes', logo: './assets/images/tech/kubernetes.svg' },
-  { name: 'Vagrant', logo: './assets/images/tech/vagrant.svg' },
-  { name: 'Terraform', logo: './assets/images/tech/terraform.svg' },
-  { name: 'Excel VBA', logo: './assets/images/tech/excel.svg' },
-  { name: 'PHP', logo: './assets/images/tech/php.svg' },
-  { name: 'MySQL', logo: './assets/images/tech/mysql.svg' },
-  { name: 'VueJS', logo: './assets/images/tech/vuejs.svg' },
-  { name: 'Asterisk', logo: './assets/images/tech/asterisk.svg' },
+const tools = [
+  { logo: './assets/images/tech/linux.svg', name: 'Linux' },
+  { logo: './assets/images/tech/ansible.svg', name: 'Ansible' },
+  { logo: './assets/images/tech/openstack.svg', name: 'OpenStack' },
+  { logo: './assets/images/tech/openshift.svg', name: 'OpenShift' },
+  { logo: './assets/images/tech/aws.svg', name: 'AWS' },
+  { logo: './assets/images/tech/git.svg', name: 'Git' },
+  { logo: './assets/images/tech/github.svg', name: 'GitHub' },
+  { logo: './assets/images/tech/gitlab.svg', name: 'GitLab' },
+  { logo: './assets/images/tech/docker.svg', name: 'Docker' },
+  { logo: './assets/images/tech/kubernetes.svg', name: 'Kubernetes' },
+  { logo: './assets/images/tech/vagrant.svg', name: 'Vagrant' },
+  { logo: './assets/images/tech/terraform.svg', name: 'Terraform' },
+  { logo: './assets/images/tech/excel.svg', name: 'Excel VBA' },
+  { logo: './assets/images/tech/php.svg', name: 'PHP' },
+  { logo: './assets/images/tech/java.svg', name: 'Java' },
+  { logo: './assets/images/tech/mysql.svg', name: 'MySQL' },
+  { logo: './assets/images/tech/vuejs.svg', name: 'VueJS' },
+  { logo: './assets/images/tech/asterisk.svg', name: 'Asterisk' },
+  { logo: './assets/images/tech/jira.svg', name: 'Jira' },
+  { logo: './assets/images/tech/linode.svg', name: 'Linode' },
+  { logo: './assets/images/tech/sendgrid.svg', name: 'SendGrid' },
 ];
 
-stacks.forEach(({ name, logo }) => {
+tools.forEach(({ name, logo }) => {
   const div = document.createElement("div");
-  div.className = "stack-icon";
+  div.className = "tools-icon";
   
   fetch(logo)
     .then(res => res.text())
@@ -31,5 +35,5 @@ stacks.forEach(({ name, logo }) => {
       div.appendChild(p);
     })
 
-  document.querySelector(".stack-grid").appendChild(div)
+  document.querySelector(".tools-grid").appendChild(div)
 });
