64f0cd50950183629eea70ae3a153fb0cc791022
Solomon <soloabejo@gmail.com>
2025-06-29 02:17:05 +0800

    feat: Miscellaneous UI and animation improvements
    Key changes:
    
    -   **Updated Lottie Player Version:** Updated the Lottie player version to 2.0.12.
    -   **Splash Screen Timeout:** Adjusted the splash screen timeout to 1.6 seconds to match the updated splash animation duration.
    -   **Tool Icon Styling:** Added `display: block` and `margin-bottom: 10px` to tool icons for improved layout.
    -   **SVG Optimization:** Removed unnecessary XML processing instructions from `bash.svg`.

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -2,7 +2,17 @@ requestAnimationFrame(() => {
     document.getElementById("splash").classList.add("visible");
 });
 
+// original splash has 50 frames for 2 seconds (40ms per frame)
+// splash was updated to 40 frames which adds up to 1.6 seconds
 setTimeout(() => {
     document.getElementById("splash").remove();
     document.querySelector('main').classList.remove("blurred");
-}, 2450);
+}, 1600);
+
+// const splash = document.getElementById("splash");
+// const splashImg = document.getElementById("splashImg");
+
+// splashImg.addEventListener("complete", () => {
+//   splash.remove();
+//   document.querySelector("main").classList.remove("blurred");
+// });
